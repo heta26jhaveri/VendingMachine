63162b9592bbb536743a495a774b6043a109677b
Heta Jhaveri <heta26jhaveri@gmail.com>
2018-11-25 12:41:23 -0500

    Minor refactorings

--- src/vengingmachine/core/service/VendingMachineTest.java
+++ src/vengingmachine/core/service/VendingMachineTest.java
@@ -13,24 +13,26 @@ import vengingmachine.core.exception.NotFullAmountPaidException;
 import vengingmachine.core.predefined.Coin;
 import vengingmachine.core.predefined.Item;
 
+/**
+ * Junit test class - provides some simple tests
+ * @author jhaveri
+ *
+ */
 public class VendingMachineTest{
 	static VendingMachine vm;
 	
 	@BeforeClass
 	public static void setUp(){
-		System.out.println("Setting up vending machine");
 		vm = new VendingMachine();
 	}
 
 	@AfterClass
 	public static void tearDown(){
-		System.out.println("Tear down vending machine");
 		vm = null;
 	}
 	
 	@Test
 	public void testCheckPrice(){
-		System.out.println("Check Item Price");
 		Item coke = Item.Coke;
 		float cokePrice = vm.selectItemAndGetPrice(coke);
 		Assert.assertEquals(cokePrice, coke.getPrice(), 0);
@@ -38,9 +40,7 @@ public class VendingMachineTest{
 	
 	@Test 
 	public void testChange(){
-		System.out.println("Check change coins");
 		Item coke = Item.Coke;
-		float cokePrice = vm.selectItemAndGetPrice(coke);
 		vm.insertCoin(Coin.Toonie);
 		float inserted = Coin.Toonie.getDenomination();
 		float remaining = inserted - coke.getPrice();
@@ -50,15 +50,13 @@ public class VendingMachineTest{
 		for(Coin coin : coins){
 			change+=coin.getDenomination();
 		}
-		
 		Assert.assertEquals(change, remaining, 0);
 	}
 	
 	@Test (expected = NotFullAmountPaidException.class)
 	public void testNotFullPaidException(){
-		System.out.println("Not full paid");
 		Item coke = Item.Coke;
-		float cokePrice = vm.selectItemAndGetPrice(coke);
+		vm.selectItemAndGetPrice(coke);
 		vm.insertCoin(Coin.Quarter);
 		vm.collectItemAndChange();
 	}
